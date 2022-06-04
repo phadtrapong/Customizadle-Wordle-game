27ae700a0ac15d2c3819d99733907c9a78bd6879
phadtrapong <phadtrapong.s@gmail.com>
2022-06-04 02:16:29 +0000

    Add typing word function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'; 
+import Wordle from './components/Wordle';
 
 function App() {
   const [solution, setSolution] = useState(null)
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
-      {solution && <div>{solution}</div>}
+      {solution && <Wordle solution={solution}/>}
     </div>
   )
 }
